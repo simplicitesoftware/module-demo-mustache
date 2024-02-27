badf017d41a27bfe0df2cd531863f11d8e2bbb1b
designer (Designer) <simplicite@simplicite.fr>
2024-02-27 12:51:21 +0100

    Modernized JS syntax

--- resources/ObjectExternal/DemoMustacheFrontend/SCRIPT.js
+++ resources/ObjectExternal/DemoMustacheFrontend/SCRIPT.js
@@ -1,35 +1,25 @@
-var DemoMustacheFrontend = DemoMustacheFrontend || (() => {
-	let app, prd;
+class DemoMustacheFrontend {
 
-	/**
-	 * Render
-	 * @param params Parameters
-	 * @function
-	 */
-	function render(params) {
+	static render(params) {
 		try {
 			if (typeof Mustache === 'undefined') throw 'Mustache not available';
-			const div = $('#demomustachefrontend');
-			if (!params.pub) div.css('min-height', '1000px');
 
-			app = app || (params.pub ?
-				// External
-				new Simplicite.Ajax(params.root, 'api', 'website', 'simplicite') :
-				// Internal
-				$ui.getAjax()
-			);
-	
-			prd = prd || app.getBusinessObject('DemoProduct');
+			const app = params.pub ?
+				// External use (public)
+				new Simplicite.Ajax(params.root, 'uipublic') :
+				// Internal UI use
+				$ui.getAjax();
+
+			const prd = app.getBusinessObject('DemoProduct');
 			prd.bannerURL = prd.bannerURL || params.bannerURL; // Image banner URL
 			prd.toFixed = prd.toFixed || function() { return (n, r) => { return parseFloat(r(n)).toFixed(2); }; }; // Rendering function for decimal
 
-			prd.search(() => {
-				div.html(Mustache.render($('#demo-template').html(), prd));
-			}, null, { inlineDocs: true });
+			const div = $('#demomustachefrontend');
+			if (!params.pub) div.css('min-height', '1000px');
+			prd.search(() => div.html(Mustache.render($('#demo-template').html(), prd)), null, { inlineDocs: true });
 		} catch(e) {
 			console.error('Render error: ' + e.message);
 		}
 	}
 
-	return { render: render };	
-})();
+}
